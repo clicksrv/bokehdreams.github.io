52226287e44876798996b40adb11fd6d4971a9b6
clicksrv <srv.k.s.11@gmail.com>
2020-04-04 22:28:13 +0530

    insta link fix

--- js/main.js
+++ js/main.js
@@ -244,7 +244,6 @@ jQuery(document).ready(function($) {
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
       var hash = this.hash;
         $('html, body').animate({
           'scrollTop': $(hash).offset().top
